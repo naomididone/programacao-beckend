dc06967ca25e30b907ed55e3382cd2cf288458cc
naomididone <naomi.didone@escola.pr.gov.br>
2025-07-30 19:35:09 +0000

    aula 09 parte 3

--- src/main/java/com/mycompany/Aula03Operadores.java
+++ src/main/java/com/mycompany/Aula03Operadores.java
@@ -1,27 +1,72 @@
-public class Aula090bjetos {
-    public static class Carro {
+package com.mycompany;
+
+public class Aula09Objetos {
+
+    public static void main(String[] args) {
         /*
-         * This is a comment block, likely for explaining the class or its members.
+         * Em java, toda classe ou tipo abstrato é um objeto, pois possuem
+         * caracteristicas
+         * e funcionalidades.
+         * 
+         * Eles herdam funções da classe mãe java.lang.Object, é correto afirmar que
+         * java.lang.Object é uma super classe
+         * de qualquer outra classe ou tipo abstrato.
+         */
+        Integer numero = 10; // Tipo abstrato
+        int numero2 = 10; // Tipo primitivo
+
+        // Retorna se a variável/classe é uma instância de outra classe
+        System.out.println(numero instanceof Object);
+
+        // Método compareTo pertence ao tipo abstrato Integer
+        numero.compareTo(numero2);
+
+        // Instânciando uma classe
+        Carro carro = new Carro("Celta", "chevrolet");
+        carro.andar(60);
+       
+        Carro carro2 = new Carro( "Celta",  "Chevrolet");
+
+        System.out.println(carro == carro2);
+
+        System.out.println(carro instanceof Carro);
+        System.out.println(carro instanceof Object);
+    }
+
+    /*
+     * Criando uma classe dentro de outra classe
+     *
+     * é necessário utilizar a instrução static
+     * entre public e class
+     */
+    public static class Carro {
+        // Atributos da classe
+        String modelo;
+        String marca;
+        int ano;
+        int quilometragem = 0;
+
+        /**
+         * Método construtor
+         *
+         * ele é chamado quando criamos uma nova instância
+         * da classe.
          */
         public Carro(String modelo, String marca) {
             this.modelo = modelo;
             this.marca = marca;
         }
 
-        // Ações da classe (Class Actions)
+        // Ações da classe
         void andar(int km) {
-            this.quilometragem += km; // Assuming 'quilometragem' (mileage) is a field of the Carro class
+            this.quilometragem += km;
             System.out.println("O carro está percorrendo " + km + "km");
+
         }
+
         void parar() {
-    System.out.println("O veículo parrou !! E percorreu um total de " + this.quilometragem + "km");
-        Carro carro = new Carro(modelo: "Celta", marca: "Chevrolet");  for constructor arguments.                                    
-        carro.andar(km: 60); 
-        Carro carro2 = new Carro(modelo: "Celta", marca: "Chevrolet"); 
-        System.out.println(carro == carro2);
-System.out.println(carro instanceof Carro);
-System.out.println(carro instanceof Object);
-}
+            System.out.println("O veículo parou !! e percorreu um total de " + this.quilometragem + "Km");
+        }
     }
 }
 
